332155f8b27b2147fa5475cbb2edb981189dcc2d
anuragkmr6 <anuragkmr6@gmail.com>
2019-05-20 09:22:59 +0530

    Implementation of STACK using array.

---
+++ DataStructure/MyStack.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+
+namespace DataStructure
+{
+    public class MyStack
+    {
+        public int[] Contents { get; set; }
+        public int Top { get; set; } = -1;
+
+        public MyStack(int size)
+        {
+            if (size > 0)
+            {
+                Contents = new int[size];
+            }
+        }
+        public void Push(int data)
+        {
+            Top++;
+            Contents[Top] = data;
+        }
+
+        public int Pop()
+        {
+            return Contents[Top--];
+        }
+
+        public void PrintAll()
+        {
+            if(Top == -1)
+            {
+                WriteLine("Stack is empty.");
+                return;
+            }
+            int top = Top;
+            while(top >= 0)
+            {
+                WriteLine(Contents[top--]);
+            }
+        }
+    }
+}
