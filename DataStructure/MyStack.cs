3a0926bbfc6aae3ed143432755b992ec7610c83a
anuragkmr6 <anuragkmr6@gmail.com>
2019-06-11 13:57:00 +0530

    Implemented: 1. Sorting of stack elements 2. Decimal to Binary conversion using stack 3.Delete even numbers of given stack

--- DataStructure/MyStack.cs
+++ DataStructure/MyStack.cs
@@ -6,7 +6,10 @@ namespace DataStructure
     {
         public int[] Contents { get; set; }
         public int Top { get; set; } = -1;
-
+        public int Count()
+        {
+            return Top + 1;
+        }
         public MyStack(int size)
         {
             if (size > 0)
@@ -19,24 +22,80 @@ namespace DataStructure
             Top++;
             Contents[Top] = data;
         }
-
         public int Pop()
         {
             return Contents[Top--];
         }
+        public int Peek()
+        {
+            return Contents[Top];
+        }
+        public MyStack Sort()
+        {
+            var tempStack = new MyStack(Count());
+            while (Count() > 0)
+            {
+                var data = Pop();
+                while (tempStack.Count() > 0 && tempStack.Peek() > data)
+                {
+                    Push(tempStack.Pop());
+                }
 
+                tempStack.Push(data);
+
+            }
+            return tempStack;
+        }
         public void PrintAll()
         {
-            if(Top == -1)
+            if (Top == -1)
             {
                 WriteLine("Stack is empty.");
                 return;
             }
             int top = Top;
-            while(top >= 0)
+            while (top >= 0)
             {
-                WriteLine(Contents[top--]);
+                Write($"{Contents[top--]} ");
             }
+            WriteLine();
+        }
+
+        public static void DecimalToBinary(int n)
+        {
+            WriteLine($"Binary conversion of {n} is:");
+
+            var stack = new MyStack(10);
+
+            while(n>0)
+            {
+                stack.Push(n % 2);
+                n = n / 2;
+            }
+                        
+            stack.PrintAll();
+        }
+
+        public void DeleteEvenNumbers()
+        {
+            var temp = new MyStack(Count());
+
+            while(Top>=0)
+            {
+                var data = Pop();
+                if(data%2==1)
+                {
+                    temp.Push(data);
+                }
+            }
+
+            while(temp.Top>=0)
+            {
+                Push(temp.Pop());
+            }
+
+            WriteLine("After deleting all even numbers from stack:");
+            PrintAll();
         }
     }
 }
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -7,17 +7,19 @@ namespace DataStructure
     {
         private static void Main(string[] args)
         {
+            #region Singly Linked List
+            /*
             SinglyLinkedList list = new SinglyLinkedList();
             for (int i = 1; i <= 5; i++)
             {
                 list.Add(i);
             }
-            //list.Add(1);
-            //list.Add(2);
-            //list.Add(1);
+            list.Add(1);
+            list.Add(2);
+            list.Add(1);
             WriteLine("Original Linked list:");
             list.Print();
-            /*
+            
             list.AddAtStart(6);
             WriteLine("\nAfter adding node at first");
             list.Print();
@@ -53,8 +55,27 @@ namespace DataStructure
             WriteLine($"Linked list count: {list.Count()}");
 
             WriteLine($"Linked list is palindrome: {list.IsPalindrome()}");
-            */
+            
             list.PrintFromLast(5);
+            */
+            #endregion
+            #region Stack
+            var stack = new MyStack(5);
+            stack.Push(1);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(4);
+            WriteLine("Data in stack:");
+            stack.PrintAll();
+            //var data = stack.Pop();
+            //WriteLine($"Popped data is: {data}");
+            //var sortedStack = stack.Sort();
+            //WriteLine("Data in stack after Sorting");
+            //sortedStack.PrintAll();
+            //MyStack.DecimalToBinary(5);
+            stack.DeleteEvenNumbers();
+            #endregion
             ReadLine();
 
         }
